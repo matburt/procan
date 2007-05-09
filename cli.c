787f7ec7644c25d919bd62f7acc1bda8d340f3a0
umeboshi <umeboshi>
2007-05-09 03:42:37 +0000

    added #include <signal.h> for linux targets

--- cli.c
+++ cli.c
@@ -12,6 +12,10 @@
 #include "procan.h"
 #include "cli.h"
 
+#if defined (linux)
+#include <signal.h>
+#endif
+
 /* Interactive mode remains in the foreground and recieves commands from stdin
  * it has the same functionality as far as backends as the daemon mode
  * but does not detach */
