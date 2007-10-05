d9719dbe5295ab7aab7cf76ad67af2adb147feb2
Matthew Jones <mat@matburt.net>
2007-10-04 23:09:26 -0400

    Initial Curses Implementation

--- cli.c
+++ cli.c
@@ -34,48 +34,28 @@
 #include <sys/param.h>
 #include <pthread.h>
 #include <unistd.h>
-#include "procan.h"
-#include "cli.h"
+#include <curses.h>
+#include <panel.h>
 #include <signal.h>
 #include <sys/ioctl.h>
-
-int get_screenheight()
-{
-  struct winsize screen;
-  if (ioctl(2,TIOCGWINSZ, &screen) == 0)
-    {
-      if (screen.ws_row > 0)
-        return screen.ws_row;
-      else
-        return 25;
-    }
-  else
-    return 25;
-}
-
-int get_screenwidth()
-{
-  struct winsize screen;
-  if (ioctl(2,TIOCGWINSZ, &screen) == 0)
-    {
-      if (screen.ws_col > 0)
-        return screen.ws_col;
-      else
-        return 80;
-    }
-  else
-    return 80;
-}
+#include "procan.h"
+#include "cli.h"
 
 /* Interactive mode remains in the foreground and recieves commands from stdin
  * it has the same functionality as far as backends as the daemon mode
  * but does not detach */
 int interactive_mode()
 {
-  pthread_t *threads;
-  char lcommand;
-  int i,e;
+  WINDOW *proc_win;
+  WINDOW *user_win;
+  PANEL *procpanel;
+  PANEL *userpanel;
 
+  pthread_t *threads;
+  char *procline;
+  int i,e, inp;
+  int startx, starty, width, height;
+  
   /* The 2 signals we watch for, and ignore the return value of children */
   signal(SIGCHLD, SIG_IGN);
   signal(SIGHUP, handle_sig);
@@ -93,45 +73,91 @@ int interactive_mode()
     printf("collector experienced a pthread error: %i\n",e);
   if (( e = pthread_create(&threads[1], NULL, analyzer_thread, NULL)) != 0)
     printf("analyzer experienced a pthread error: %i\n",e);
- 
-  while ((lcommand = getchar()) != 'q' && m_hangup != 1)
+
+  /* Start Curses */
+  initscr();			
+  cbreak();			
+  keypad(stdscr, TRUE);
+  noecho();
+  start_color();
+  init_pair(1, COLOR_WHITE, COLOR_BLACK);
+  init_pair(2, COLOR_CYAN, COLOR_BLACK);
+  init_pair(3, COLOR_MAGENTA, COLOR_WHITE);
+  init_pair(4, COLOR_WHITE, COLOR_BLUE);
+  bkgd(COLOR_PAIR(1));
+  refresh();
+  
+  /* get the full screen's coordinate */
+  getmaxyx(stdscr, height, width);
+  startx = 0;
+  starty = 0;
+  
+  proc_win = newwin(height/2, width, starty, startx);
+  user_win = newwin(height/2, width, starty+(height/2), startx);
+
+  procpanel = new_panel(proc_win);
+  userpanel = new_panel(user_win);
+
+  update_panels();
+  doupdate();
+  nodelay(proc_win, true);
+  nodelay(user_win, true);
+
+
+  int refreshcounter = 500;
+
+  // case KEY_VALUE_CAPITAL_Q: /* Quit */
+  //case KEY_VALUE_LOWER_Q: 113
+  while ((inp = wgetch(proc_win)) != 113 && m_hangup != 1)
     {
-      if (lcommand == 'p')
-	{
-	  struct timeval *now = (struct timeval *)malloc(sizeof(struct timeval));
-	  gettimeofday(now,NULL);
-	  pthread_mutex_lock(&procchart_mutex);
-	  printf("command -- lastpid -- lasttime -- num_seen -- mov_percent -- size_gain -- rssize_gain -- #measures -- interest -- num interests\n");
-	  for (i = 0; i < numprocavs; i++)
-	    {
-	      if (procavs[i].last_measure_time > now->tv_sec - 30)
-		{
-		  printf("%s -- %i -- %i -- %li -- %i -- %i -- %i -- %i -- %i -- %i -- %i\n", 
-			 procavs[i].command,
-			 procavs[i].uid,
-			 procavs[i].lastpid,
-			 procavs[i].last_measure_time,
-			 procavs[i].num_seen,
-			 procavs[i].mov_percent,
-			 procavs[i].avg_size_gain,
-			 procavs[i].avg_rssize_gain,
-			 procavs[i].times_measured,
-			 procavs[i].intrest_score,
-			 procavs[i].num_intrests);
-		}
-	    }
-	  free(now);
-	  pthread_mutex_unlock(&procchart_mutex);
-	}
-      else if (lcommand == 's')
-	{
+      refreshcounter = 500;
+      struct timeval *now = (struct timeval *)malloc(sizeof(struct timeval));
+      gettimeofday(now,NULL);
+      pthread_mutex_lock(&procchart_mutex);
+      
+      wclear(proc_win);
+      wclear(user_win);
+      mvwaddstr(proc_win, 1, 1, "---=Active Processes=---");          
+      mvwaddstr(user_win, 1, 1, "---=Active Users=---");
+      
+      mvwaddstr(proc_win, 2, 1, "command | lastpid | mov_percent | size_gain | rssize_gain | interest | #interest");
+      for (i = 0; i < numprocavs; i++)
+        {
+          if (procavs[i].last_measure_time > now->tv_sec - 30)
+            {
+              procline = (char *)malloc(100*sizeof(*procline));
+              snprintf(procline, 100, "%s %i %i %i %i %i %i", 
+                       procavs[i].command,
+                       //procavs[i].uid,
+                       procavs[i].lastpid,
+                       //procavs[i].last_measure_time,
+                       //procavs[i].num_seen,
+                       procavs[i].mov_percent,
+                       procavs[i].avg_size_gain,
+                       procavs[i].avg_rssize_gain,
+                       //procavs[i].times_measured,
+                       procavs[i].intrest_score,
+                       procavs[i].num_intrests);
+              mvwaddstr(proc_win, (i+3), 1, procline);
+              free(procline);
+            }
+          wrefresh(proc_win); 
+          wrefresh(user_win); 
+        } 
+      
+      free(now);
+      pthread_mutex_unlock(&procchart_mutex);
+      /* else if (lcommand == 's')
+         {
 	  printf("Showing the top 5 most interesting active processes and why:\n");
 	  char *info = get_statistics();
 	  printf("%s", info);
-	  //free(info); /* This seems to cause corrupted redzones, not sure why */
-	}
+	  free(info); This seems to cause corrupted redzones, not sure why
+          } */
+      sleep(1);
     }
 
+  endwin();
   pthread_mutex_lock(&hangup_mutex);
   m_hangup=1;
   pthread_mutex_unlock(&hangup_mutex);
@@ -157,7 +183,7 @@ int interactive_mode()
 	free(procavs[i].command);
     }
   free(procavs);
-
+  
   return 0;
 }
 
