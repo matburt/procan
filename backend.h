f7176f3f52b5b54cfec070291d954771a41d192f
bsdmatburt <bsdmatburt@8a20e897-151a-0410-b199-451139a771d9>
2008-06-15 01:45:03 +0000

    Flushing repository in order to utilize git-svn
    git-svn-id: https://procan.googlecode.com/svn/trunk@40 8a20e897-151a-0410-b199-451139a771d9

--- backend.h
+++
@@ -1,9 +0,0 @@
-extern pthread_mutex_t procchart_mutex;
-extern proc_averages *procavs;
-extern int numprocavs;
-
-int syslog_backend(procan_config *pc, struct timeval *schedtime);
-int mail_backend(procan_config *pc, struct timeval *schedtime);
-int script_backend(procan_config *pc);
-int get_warns(int *indcs, procan_config *pc, int backendtype);
-int get_alarms(int *indcs, procan_config *pc, int backendtype);
